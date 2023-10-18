7bd6c470eebe9789758dfbdd0a9887cca2c38bf5
Juan Carlos <montilla269@gmail.com>
2023-10-18 19:06:50 -0300

    fix fn_get

--- api/user.py
+++ api/user.py
@@ -7,9 +7,7 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.end_headers()
-        with open('index.html', 'r') as f:
-            response = f.read()
-        self.wfile.write(response.encode('utf-8'))
+        self.wfile.write('hola desde get'.encode('utf-8'))
         return 
     
     
