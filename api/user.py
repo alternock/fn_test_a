c06884e8e2f8ba3ea1a18324b4f856eabb8ff12b
Juan Carlos <montilla269@gmail.com>
2023-10-18 18:39:55 -0300

    fc

---
+++ api/user.py
@@ -0,0 +1,36 @@
+import json
+from http.server import BaseHTTPRequestHandler
+
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        with open('index.html', 'r') as f:
+            response = f.read()
+        self.wfile.write(response.encode('utf-8'))
+        return 
+    
+    
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header('Content-type','application/json')
+        self.end_headers()
+        self.wfile.write('hola desde post'.encode('utf-8'))
+        return 
+    
+    
+    def do_PUT(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write('hola desde put'.encode('utf-8')) 
+        return    
+            
+    def do_DELETE(self):        
+        self.send_response(200)
+        self.send_header('Content-type','application/json')
+        self.end_headers()
+        self.wfile.write('hola desde delete'.encode('utf-8'))
+        return     
